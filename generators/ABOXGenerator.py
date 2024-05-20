4a9c3d55e1cb2835c723d07edc74a15cf365565f
airdac <nouacl@gmail.com>
2024-05-20 10:12:44 +0200

    more changes

--- generators/ABOXGenerator.py
+++ generators/ABOXGenerator.py
@@ -1,6 +1,6 @@
 # SDM Project 2. Knowledge Graphs
 # ABOX generator
-import pandas as pd  # for handling csv and csv contents
+from pandas import read_csv  # for handling csv and csv contents
 from rdflib import Graph, Namespace, Literal, URIRef  # basic RDF handling
 import os
 import os.path as op
@@ -22,7 +22,7 @@ class ABOXGenerator():
         data_path = op.join(cwd, 'data')
         nodes_path = op.join(data_path, 'nodes')
 
-        df_affiliation = pd.read_csv(
+        df_affiliation = read_csv(
             op.join(nodes_path, 'Node_affiliation.csv')
             , sep=',', header=0)
         
@@ -34,11 +34,26 @@ class ABOXGenerator():
             node_uri  = URIRef(n + ('affiliation_' + str(index)))
             g.add((node_uri, n.affiliationHasType, Literal(aff['Type'])))
             g.add((node_uri, n.affiliationHasName, Literal(aff['Affiliation'])))
+
+        df_author = read_csv(
+            op.join(nodes_path, 'Node_author.csv')
+            , sep=',', header=0)
+        print(df_author.head())
+        print('Columns:', df_author.columns.to_list())
+
+        for index, auth in df_author.iterrows():
+            if index >= 10: break
+            node_uri = URIRef(n + ('author_' + str(index)))
+            g.add((node_uri, n.authorHasName, Literal(auth['author'])))
         ######################################
 
 
         ######## Edge generation test ########
+        edges_path = op.join(data_path, 'edges')
 
+        df_aff_auth = read_csv(
+            op.join()
+        )
         ######################################
         
         # Generate .ttl
